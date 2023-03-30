fa019e92aa0050c3b0dd7fafda91016630534126
Bizgrell <dratutiyawkaf@gmail.com>
2023-03-30 20:25:44 +0300

    lab3

--- Tweb/src/App.tsx
+++ Tweb/src/App.tsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import DataTable from './datatable.tsx';
+import SimpleCard from './card.tsx';
 
 function App() {
+
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div>
        <p> Malanciuc Dmitri Cr-203
       </p>
+    <DataTable/>
+    <SimpleCard/>
     </div>
   )
 }
